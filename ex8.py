c43f9372cb7b42a7d91e26abc2a4b65dcd383c93
ibomb23 <i.helmstaedt@gmail.com>
2017-03-07 21:54:20 +0100

    Update ex8.py
    Add the remaining comments

--- ex8.py
+++ ex8.py
@@ -3,8 +3,8 @@ formatter = "%r %r %r %r"                                         # set the vari
 print formatter % (1, 2, 3, 4)                                    # print out the variable "formatter" with the formatters "1", "2", "3", "4"
 print formatter % ("one", "two", "three", "four")                 # print out the variable "formatter" with the formatters "one", "two", "three", "four" 
 print formatter % (True, False, False, True)                      # print out the variable "formatter" with the formatters "True", "False", "False", "True"
-print formatter % (formatter, formatter, formatter, formatter)    #
-print formatter % (
+print formatter % (formatter, formatter, formatter, formatter)    # print out the variable "formatter" with the formatters "formatter", "formatter", "formatter", "formatter"
+print formatter % (                                               # print out the variable "formatter" with the four sentences as formatters
   "I had this thing.",
   "That you could type up right.",
   "But it didn't sing.",
