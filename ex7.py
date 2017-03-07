5aa6cc07527870dd32fe8e2449bd73c5b16bddf9
ibomb23 <i.helmstaedt@gmail.com>
2017-03-07 09:47:23 +0100

    Update ex7.py
    Set comments to the whole document

--- ex7.py
+++ ex7.py
@@ -1,22 +1,22 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10 # what'd that do?
+print "Mary had a little lamb."                 # print out a string
+print "Its fleece was white as %s." % 'snow'    # print out a string with a formater string 
+print "And everywhere that Mary went."          # print out a string
+print "." * 10 # what'd that do?                # print out a string - ten times
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = "C"                                      # set the variable "end1" to the char "C"
+end2 = "h"                                      # set the variable "end2" to the char "h"
+end3 = "e"                                      # set the variable "end3" to the char "e"
+end4 = "e"                                      # set the variable "end4" to the char "e"
+end5 = "s"                                      # set the variable "end5" to the char "s"
+end6 = "e"                                      # set the variable "end6" to the char "e"
+end7 = "B"                                      # set the variable "end7" to the char "B"
+end8 = "u"                                      # set the variable "end8" to the char "u"
+end9 = "r"                                      # set the variable "end9" to the char "r"
+end10 = "g"                                     # set the variable "end10" to the char "g"
+end11 = "e"                                     # set the variable "end11" to the char "e"
+end12 = "r"                                     # set the variable" end12" to the char "r"
 
 # watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
-print end7 + end8 + end9 + end10 + end11 + end12
+print end1 + end2 + end3 + end4 + end5 + end6,    # print out the sum of the strings "end1" - "end6" - with the comma the strings will print in the same line
+print end7 + end8 + end9 + end10 + end11 + end12  # print out the sum of the stríngs "end7" - "end12" 
 
