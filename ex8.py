83952a679b1b807f6df9a16c5a30424941a178a8
ibomb23 <i.helmstaedt@gmail.com>
2017-03-07 11:02:54 +0100

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "%r %r %r %r"                                         # set the variable "formatter" to a string with four raw-formatter signs
+
+print formatter % (1, 2, 3, 4)                                    # print out the variable "formatter" with the formatters "1", "2", "3", "4"
+print formatter % ("one", "two", "three", "four")                 # print out the variable "formatter" with the formatters "one", "two", "three", "four" 
+print formatter % (True, False, False, True)                      # print out the variable "formatter" with the formatters "True", "False", "False", "True"
+print formatter % (formatter, formatter, formatter, formatter)    #
+print formatter % (
+  "I had this thing.",
+  "That you could type up right.",
+  "But it didn't sing.",
+  "So I said goodnight."
+  )
