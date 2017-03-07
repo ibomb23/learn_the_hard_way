3caf6fcd85c8538cc6d0d976ada2df5ec509be3c
ibomb23 <i.helmstaedt@gmail.com>
2017-03-07 21:49:05 +0100

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:"   #show the message that i want to count my chickens
+
+print "Hens", 25+30 / 6                 #print out "Hens" and the result of 25+30 / 6
+print "Roosters", 100 - 25 * 3 % 4      #print out "Roosters" and the reslt of 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"      #show the messagge that i want to count my eggs
+
+print 3 + 2 +1 - 5 + 4 % 2 - 1 / 4 + 6  #print out the result of 3 + 2 +1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"  #show the question that 3 + 2 < 5 - 7 is true
+
+print 3 + 2 < 5 - 7                     #print out the boolean value of the term 3 + 2 < 5 - 7
+
+print "What is 3 + 2?" , 3+2            #print out the question for the sum of 3 + 2 and the result
+print "What is 5 - 7?" , 5 - 7          #print out the question for the result of 5 - 7 and the result
+
+print "Oh, that's why it's False."      #print out the answer to the question above
+
+print "How about some more."            #print out to see more print- and math-stuff
+
+print "Is it greater?", 5 > (-2)         #print out a question and the boolean value of the question
+print "It is greater or equal?", 5 >= -2 #print out a question and the boolean value of the question
+print "It is  less or equal?", 5 <= -2   #print out a question and the boolean value of the question
